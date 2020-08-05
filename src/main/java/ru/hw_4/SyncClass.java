f7d9c3fb844c2c4124dec00ed17fea53e931aaa9
Max <moriv08@yandex.ru>
2020-08-05 12:56:48 +0300

    lesson_4_part_2

--- src/main/java/ru/hw_4/SyncClass.java
+++ src/main/java/ru/hw_4/SyncClass.java
@@ -2,9 +2,25 @@ package ru.hw_4;
 
 public class SyncClass {
 
+    char curent = 'A';
+
     protected synchronized void printLetter(MyThread t){
 
-            System.out.print(t.s);
+
+        if (t.s != curent) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.print(t.s);
+
+        if (curent == 'B')
+            curent -= 2;
+        else
+            curent += 1;
 
         try {
             notifyAll();
